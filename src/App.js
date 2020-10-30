91423b7d6526c13ae9f026985e39da7be3f1c8e6
Clannis <Dodds1990@gmail.com>
2020-10-30 12:04:19 -0500

    add header and first collumns row too App component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import { Component } from 'react'
 
-function App() {
+class App extends Component {
+  render() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <section class="hero is-primary">
+        <div class="hero-body ">
+          <div class="container">
+            <h1 class="title">
+              Bulma Practice Site
+            </h1>
+            <h2 class="subtitle">
+              Pulled from within App Component
+            </h2>
+          </div>
+        </div>
+      </section>
+      <section class="section">
+        <div class="container">
+          <div class="columns is-vcentered">
+            <div class="column">
+              <h1 class="title">
+               Information
+              </h1>
+              <h2 class="subtitle">
+                Short Summary of Information
+              </h2>
+              <div class="buttons">
+                <a class="button is-info">Find out more</a>
+                <a class="button is-danger is-outlined">Buy now</a>
+              </div>
+            </div>
+            <div class="column">
+              <figure class="image is-16by9">
+                <img src="https://www.dataiku.com/wp-content/uploads/2019/10/chris-ried-ieic5Tq8YMk-unsplash-1618x1080.jpg" alt="code"/>
+              </figure>
+            </div>
+          </div>
+        </div>
+      </section>
     </div>
-  );
+    );
+  }
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -3,10 +3,21 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { Provider } from 'react-redux'
+import thunk from 'redux-thunk'
+import { createStore, applyMiddleware} from 'redux'
+import {BrowserRouter as Router} from 'react-router-dom'
+import reducer from './reducer/reducer'
+
+let store = createStore(reducer, applyMiddleware(thunk))
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <Router>
+        <App />
+      </Router>
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
